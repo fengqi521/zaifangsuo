06db4a383128159dadb9ca24ab3fcfc65d71edbd
fengqi521 <zhangheng_037@163.com>
2024-07-01 16:54:39 +0800

    feat:新增登录页

---
+++ src/components/ElIconInput/index.vue
@@ -0,0 +1,8 @@
+<!-- <script setup></script>
+<template>
+  <el-input v-model="input3" style="width: 240px" placeholder="Pick a date">
+    <template #suffix>
+      <el-icon class="el-input__icon"><calendar /></el-icon>
+    </template>
+  </el-input>
+</template> -->
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,2 +1,54 @@
-<template>登录页</template>
-<script setup></script>
+<script setup>
+import LoginForm from "./components/LoginForm/index.vue";
+</script>
+<template>
+  <div class="login-container">
+    <div class="login-wrapper">
+      <a class="logo"
+        ><img
+          class="logo-avatar"
+          src="@/assets/images/beijingDiKan.png"
+          alt="北京地勘"
+      /></a>
+      <div class='login-bg'></div>
+      <LoginForm />
+    </div>
+  </div>
+</template>
+<style lang="scss" scoped>
+.login-container {
+  display: flex;
+  width: 100%;
+  height: 100vh;
+  background: var(--login-bg-color);
+  overflow: hidden;
+
+  .login-wrapper {
+    position: relative;
+    width: 60%;
+    height: 75%;
+    margin: auto;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+  }
+
+  .logo {
+    position: absolute;
+    top: -8.5%;
+    left: 5%;
+    width: 110px;
+
+    .logo-avatar {
+      width: 100%;
+      height: auto;
+    }
+  }
+
+  .login-bg{
+    width: 63%;
+    height: 100%;
+    background: url(@/assets/images/login-bg.png) right 50% / contain no-repeat;
+  }
+}
+</style>
